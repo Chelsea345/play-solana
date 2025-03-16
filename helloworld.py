71ad18964af4e5c708ed10be6d9cae6ffb2fa40f
Chelsea345 <zhihuichen004@gmail.com>
2025-03-16 12:07:23 +0800

    rm

--- helloworld.py
+++
@@ -1,9 +0,0 @@
-# name = input()
-# print("qnmd",name)
-print("please input your age")
-age = int(input())
-if age > 18:
-    print('your age is', age)
-    print('you can marry')
-else:
-    print('you can not marry')
